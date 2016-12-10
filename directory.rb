c54968b5a1958b1bb5c6edc595d43a1b429f9d7d
gbhachu <gurminderbhachu@hotmail.com>
2016-12-10 17:59:42 +0000

    Exe 7 rev

--- directory.rb
+++ directory.rb
@@ -7,8 +7,13 @@ students = []
 months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" ]
 
 def print(students)
-   students.each.with_index(1) do |student, index|
-    puts "#{index}. #{student[:name]} (#{student[:cohort]} cohort)".center(60)
+
+months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" ]
+
+student_sort = students.sort_by { |student| months.index student[:cohort]}
+
+student_sort.each.with_index(1) do |x, index|
+puts "#{index} #{x[:first_name]} #{x[:surname]} (#{x[:cohort]} cohort)".center(60)
   end
 end
 
@@ -18,47 +23,37 @@ end
 
 def input_students
 
-   students = []
 
-   months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" ]
-
-  puts "Press 'any key' then enter to input student details or 'Enter' to exit"
-
+puts "Please enter the names of the students."
+puts "To finish just hit return twice."
+  students = []
   name = gets.chomp
-
+  name = name.to_sym
   while !name.empty? do
-   puts "Please enter name: "
-   name = gets.chomp.capitalize
-
-   # Checks if cohort is correct
-    cohort_month = false
-  while  cohort_month == false
-   puts "Please enter cohort: "
-   cohort = gets.chomp.capitalize
-     if !months.include? cohort
-       puts "Invalid entry please re-enter cohort"
-     else
-       cohort_month = true
-    end
+    puts "Please enter the cohort of the student."
+    cohort = gets.chomp
+    cohorts = ["January","February","March","April","May","June","July","August","September","October","November","December"]
+      if cohort.empty?
+        cohort = :no_input
+      else
+        while !cohorts.any? { |month| cohort.include? month}
+        puts "There are typos in your entry. Could you type the cohort in again please?"
+        cohort = gets.chomp
+        end
+      cohort = cohort.to_sym
+      end
+    students << {name: name, cohort: cohort}
+    puts "Now we have #{students.count} students!"
+    puts "Please enter next student."
+    name = gets.chomp
+    name = name.to_sym
   end
-
-   puts "Please enter hobbies: "
-   hobbies = gets.chomp.capitalize
-   puts "\nPlease enter country of birth: "
-   country_of_birth = gets.chomp.capitalize
-   puts "\nPlease enter height in inches: "
-   height = gets.chomp.capitalize
-
-   #add the student to the array
-   students << {name: name, cohort: cohort, hobbies: hobbies, country_of_birth: country_of_birth, heighth: height}
-     puts "Now we have #{students.count} students".center(60)
-     puts "Press 'any key' and 'Enter' to add another student or 'Enter' to exit".center(60)
-   name = gets.chomp
-   end
-     students
-
- end
-
+  if name.empty?
+    name = :no_name
+  end
+    puts "Now we have #{students.count} students!"
+    students
+end
  def print(students)
 
    # while loop to return student names
